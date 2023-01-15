10f3a45634f8942634514067697548217f51c923
KiwiGin <bayeskiwigen@gmail.com>
2023-01-14 21:52:15 -0500

    finish 1

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+const quoteContenedor=document.getElementById('quote-contenedor');
+const quoteTexto=document.getElementById('quote');
+const quoteAutor=document.getElementById('autor');
+const tuiterBtn=document.getElementById('twitter');
+const newQuoteBtn=document.getElementById('new-quote');
+
+
+let apiQuotes=[];
+
+//Mostrar nueva cita
+function nuevaCita(){
+    const quote=apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    if(!quote.author){
+        quoteAutor.textContent='Unknown';
+    }else{
+        quoteAutor.textContent=quote.author;
+    }
+
+    if(quote.text.length>110){
+        quoteTexto.classList.add('long-quote');
+    }else{
+        quoteTexto.classList.remove('long-quote');
+    }
+    quoteTexto.textContent=quote.text;
+}
+
 //Get Quotes from API
+async function geTQuotes(){
+    const apiurl = 'https://type.fit/api/quotes';
+    try{
+        const response = await fetch(apiurl);
+        apiQuotes=await response.json();
+        nuevaCita();
+    }catch(error){
+        //algo
+    }
+}
+
+//Hacer tuit
+function nuevoTweet(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteTexto.textContent} - ${quoteAutor.textContent}${'. Estoy usando esta página https://kiwigin.github.io/quoteG/'}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Events listener
+newQuoteBtn.addEventListener('click', nuevaCita);
+tuiterBtn.addEventListener('click', nuevoTweet);
+
+//On Load
+geTQuotes();
