979d09a007e542bca3403f41d0725d039f74ff0d
KiwiGin <bayeskiwigen@gmail.com>
2023-01-28 16:55:42 -0500

    finish

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteTexto=document.getElementById('quote');
 const quoteAutor=document.getElementById('autor');
 const tuiterBtn=document.getElementById('twitter');
 const newQuoteBtn=document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes=[];
 
+//show loading
+function loading() {
+    loader.hidden = false;
+    quoteContenedor.hidden=true;
+}
+
+//hide loading
+function complete(){
+    quoteContenedor.hidden=false;
+    loader.hidden=true;
+}
+
 //Mostrar nueva cita
 function nuevaCita(){
+    loading(); 
     const quote=apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
     if(!quote.author){
         quoteAutor.textContent='Unknown';
@@ -21,11 +35,14 @@ function nuevaCita(){
     }else{
         quoteTexto.classList.remove('long-quote');
     }
+    //set quote, hide loader
     quoteTexto.textContent=quote.text;
+    complete();
 }
 
 //Get Quotes from API
 async function geTQuotes(){
+    loading();
     const apiurl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiurl);
